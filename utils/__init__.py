883bd4bded686a1468f9c64f8f8d5862125ebcf2
SCHWMAX <max.schwenzer@voith.com>
2024-09-24 15:11:03 +0200

    save / load checkpoint

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,4 +2,4 @@ from utils.data import create_dataset
 
 from utils.signals import normalize_df, zero_pad_df, OnlineStats
 
-from general import save_autoencoder, load_autoencoder
+from utils.general import save_autoencoder, load_autoencoder
